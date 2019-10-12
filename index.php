43d1ce11a3ca703bd24fee3a5d4da614acaa8635
Pedro Igor <pedroigor.silva@outlook.com>
2019-10-12 01:54:32 -0300

    Criando um serviço web que receberá uma requisição GET.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$user = explode('/', $_GET['user'])[0];
+$pass = explode('/', $_GET['password'])[0];
+$method = $_SERVER['REQUEST_METHOD'];
+header('Content-type: application/json');
+
+if($method == 'GET'){
+    if(strval($user) == '"syscoin"' and $pass == '"meEscolhe"')
+        $json = file_get_contents('success.json');
+    else
+        $json = file_get_contents('failure.json');
+        
+    echo $json;
+}
