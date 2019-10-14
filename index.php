0cf4ad635144f0be441b1ecc09359539f1ab6834
Pedro Igor <pedroigor.silva@outlook.com>
2019-10-13 22:49:35 -0300

    Enviando informações juntamente com o redirecionamento.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-header("Location: provasyscoin?");
+$user = explode('/', $_GET['user'])[0];
+$pass = explode('/', $_GET['password'])[0];
+echo "Location: provasyscoin?user=".$user."&password=".$pass;
+header("Location: provasyscoin?user=".$user."&password=".$pass);
 exit(0);
 ?>
